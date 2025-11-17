90c9dddfb768621237a60caa7600f59c67eb8cd7
Inihor66 <aarohipurwar06@gmail.com>
2025-11-17 17:33:46 +0530

    Update api.js

--- api/email/api.js
+++ api/email/api.js
@@ -1,18 +1,1 @@
-// api/email/api.js
 
-export async function sendVerificationEmail(email, userId, code, expiresAt) {
-  try {
-    const res = await fetch("https://audit-management-wgbd.onrender.com/email/send-verification", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, userId, code, expiresAt })
-    });
-
-    const data = await res.json();
-    return data;
-
-  } catch (err) {
-    console.error("Email API Error:", err);
-    return { error: "Failed" };
-  }
-}
