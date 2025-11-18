9ac44712a258f68d2584cd5943f9192f300c39d5
Inihor66 <aarohipurwar06@gmail.com>
2025-11-18 11:53:00 +0530

    Update roles.js

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -3,17 +3,14 @@ import { Role } from "../types";
 export const ROLE_CONFIG = {
   [Role.FIRM]: {
     name: "Firm",
-    freeEntries: 0,
     hex: "#1E90FF",
   },
   [Role.STUDENT]: {
     name: "Student",
-    freeEntries: 0,
     hex: "#32CD32",
   },
   [Role.ADMIN]: {
     name: "Admin",
-    freeEntries: 0,
     hex: "#FF4500",
   },
 };
