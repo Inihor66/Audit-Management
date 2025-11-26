a10eba9733bd939baf99d5235b263b3ca6259eb4
Inihor66 <aarohipurwar06@gmail.com>
2025-11-26 13:51:51 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css'; // Important: Apply global styles
 
 const rootElement = document.getElementById('root');
-
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
