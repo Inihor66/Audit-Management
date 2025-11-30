77f14a9c4db2822fe3b1bebd3e36e1a87b29b6b9
Inihor66 <aarohipurwar06@gmail.com>
2025-11-30 21:36:59 +0530

    Update CheckIcon.tsx

--- components/icons/CheckIcon.tsx
+++ components/icons/CheckIcon.tsx
@@ -1,22 +1,10 @@
-import * as React from "react";
 
-function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-      />
-    </svg>
-  );
-}
+import React from 'react';
+
+const CheckIcon = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+  </svg>
+);
 
 export default CheckIcon;
