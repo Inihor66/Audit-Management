c3b5c5e595786a1604d96a2a0dcc0b6f92d69187
Inihor66 <aarohipurwar06@gmail.com>
2025-12-01 11:16:47 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,22 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("audit-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/index.css",
+        "/index.tsx",
+        "/manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
