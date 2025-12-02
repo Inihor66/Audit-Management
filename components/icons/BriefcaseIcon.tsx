37cac41053d78ae3780486574e7d29c963fc8e90
Inihor66 <aarohipurwar06@gmail.com>
2025-12-02 20:41:59 +0530

    Update BriefcaseIcon.tsx

--- components/icons/BriefcaseIcon.tsx
+++ components/icons/BriefcaseIcon.tsx
@@ -1,6 +1,8 @@
 
-export const BriefcaseIcon = ({ className }: { className?: string }) => (
+import React from 'react';
+
+export const BuildingIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
   </svg>
 );
