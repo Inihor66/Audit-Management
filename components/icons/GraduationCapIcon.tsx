863ec6ab86b65eec5a31040bfea5b0a815b3cb00
Inihor66 <aarohipurwar06@gmail.com>
2025-12-02 17:51:21 +0530

    Update GraduationCapIcon.tsx

--- components/icons/GraduationCapIcon.tsx
+++ components/icons/GraduationCapIcon.tsx
@@ -1,6 +1,4 @@
 
-import React from 'react';
-
 export const GraduationCapIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path d="M12 14l9-5-9-5-9 5 9 5z" />
