95330f8065ce1ab045cf78fd6da61139d6e8c7ad
Inihor66 <aarohipurwar06@gmail.com>
2025-12-03 18:23:14 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Styles are now loaded via index.html link
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
